c7ccea39af1aaf6ae3f1dd9846255f13e79d1b49
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-08-27 23:18:00 +0530

    order and purchase order updated

---
+++ Back_End/src/lk/ijse/jsp/servlet/PurchaseOrderServletAPI.java
@@ -0,0 +1,9 @@
+package lk.ijse.jsp.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/pages/purchase-order"})
+public class PurchaseOrderServletAPI extends HttpServlet {
+
+}
